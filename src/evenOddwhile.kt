12cc5b95f42d4c5b184890982d009c7f3a1c0122
utshoahmed000-kiki <utshoahmed000@gmail.com>
2026-01-12 19:12:38 +0600

    while loop Even Odd

--- src/evenOddwhile.kt
+++ src/evenOddwhile.kt
@@ -1,8 +1,21 @@
 fun main(){
 
-    print("Enter the number")
-    var num = readln().toInt()
+    print("Enter the number : ")
+    var num1 = readln().toInt()
 
+    var num2 =1
+
+    while (num2<=num1 ) {
+
+        if (num2 % 2 == 0) {
+            println("$num2 is Even")
+        } else {
+            println("$num2 is odd")
+
+        }
+
+        num2++
+    }
 
 
 
