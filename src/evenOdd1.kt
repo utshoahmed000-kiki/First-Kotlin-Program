2e813c938a76904643fafbcfd4968f2845393864
utshoahmed000-kiki <utshoahmed000@gmail.com>
2026-01-12 20:57:21 +0600

    For loop Even Odd

---
+++ src/evenOdd1.kt
@@ -0,0 +1,13 @@
+fun main (){
+
+    print("Enter the number : ")
+    var num=readln().toInt()
+
+    if (num % 2==0){
+        println("Number is Even")
+    }
+    else {
+        println("Number is Odd")
+    }
+
+}
