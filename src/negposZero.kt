fc9c1c4367a68380e2abaa1736cd363a9e03b48f
utshoahmed000-kiki <utshoahmed000@gmail.com>
2026-01-12 20:52:02 +0600

    For loop Positive Negative Zero

---
+++ src/negposZero.kt
@@ -0,0 +1,15 @@
+fun main (){
+     print("Enter the number : ")
+    var num=readln().toInt()
+
+    if (num>0){
+        println("Positive")
+    }
+    else if (num<0){
+        println("Negative")
+    }
+    else{
+        println("Zero")
+    }
+
+}
