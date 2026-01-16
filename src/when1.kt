554e0932e6a87d07b0bef2ded2bcdcfb0ff856db
utshoahmed000-kiki <utshoahmed000@gmail.com>
2026-01-16 15:37:52 +0600

    when grade1

--- src/when1.kt
+++ src/when1.kt
@@ -1,14 +1,14 @@
 fun main (){
 
-    print("Enter the grade(A,B,C,D,F)")
-    val grade = readLine()!!
+    print("Enter the grade(A,B,C,D,F) : ")
+    val grade = readLine()!![0]
 
     when (grade){
-        "A"-> println("Excellent")
-        "B"-> println("Good")
-        "C"-> println("Average")
-        "D"-> println("Pass")
-        "F"-> println("Fail")
+        'A'-> println("Excellent")
+        'B'-> println("Good")
+        'C'-> println("Average")
+        'D'-> println("Pass")
+        'F'-> println("Fail")
         else -> println("Invalid Grade")
     }
 
