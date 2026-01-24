f2de157cc7ae4e04475847098ad8f1e765942314
utshoahmed000-kiki <utshoahmed000@gmail.com>
2026-01-24 20:32:21 +0600

    problem

---
+++ src/practice.kt
@@ -0,0 +1,13 @@
+fun main (){
+
+    val A = readLine()!!.toInt()
+    val B = readLine()!!.toInt()
+    val C = readLine()!!.toInt()
+    val D = readLine()!!.toInt()
+
+    val difference=A*B-C*D
+    println("DIFERENCA = $difference")
+
+
+
+}
